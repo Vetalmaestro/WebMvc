1af5446e20de2e0cd249bf815c9b02090dac635d
vetalmaestro <kolomiets85@gmail.com>
2015-12-14 19:58:12 +0200

    Initial commit

---
+++ WebMvc/Controllers/AdminController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMvc.Models;
+
+namespace WebMvc.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Users()
+        {
+            List<User> users = new List<User>();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["Db"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("select UserId, Name, Password from Users", connection);
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var user = new User();
+                        user.UserId = reader.GetInt32(0);
+                        user.Name = reader.GetString(1);
+                        user.Password = reader.GetString(2);
+                        users.Add(user);
+                    }
+                }
+            }
+            return View(users);
+        }
+    }
+}
